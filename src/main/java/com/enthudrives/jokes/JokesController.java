78917b6491d90ecc2b8720fe79765899cb53c851
Suganthi <suganthik@pivotal.io>
2020-03-04 12:04:14 +1100

    Jokes demo

---
+++ src/main/java/com/enthudrives/jokes/JokesController.java
@@ -0,0 +1,21 @@
+package com.enthudrives.jokes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class JokesController {
+
+    @GetMapping("/api/jokes")
+    List<String> getJokes() {
+        List<String> jokes = new ArrayList<>();
+        jokes.add("How you doing!");
+        jokes.add("Bazinga");
+
+        return jokes;
+    }
+}
